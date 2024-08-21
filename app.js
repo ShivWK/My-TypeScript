4c32b568223d879bff03b310ba2a64b4f156815c
Admin <shivendrawk@gmail.com>
2024-08-21 21:16:50 +0530

    typeScript Start

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello shivendra");
+var gggName = "shivendra";
+console.log(gggName);
+var Id = 89;
+console.log(Id);
+var mine = "new one";
+console.log(mine);
+//}) ()
---
+++ app.ts
@@ -0,0 +1,15 @@
+console.log("hello shivendra");
+
+let gggName = "shivendra";
+
+console.log(gggName);
+
+const Id = 89;
+
+console.log(Id);
+
+let mine : string = "new one";
+
+let name = "JDJD"
+console.log(name);
+//}) ()
