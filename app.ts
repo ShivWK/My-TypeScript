4549958a51efdcee281c7dc5ad1be134b0e08d69
Admin <shivendrawk@gmail.com>
2024-08-22 22:29:44 +0530

    TSC datatypes

--- app.ts
+++ app.ts
@@ -1,15 +1,39 @@
-console.log("hello shivendra");
+export {};
+
+// console.log("hello shivendra");
 
 let gggName = "shivendra";
 
-console.log(gggName);
+// console.log(gggName);
 
 const Id = 89;
 
-console.log(Id);
+// console.log(Id);
 
 let mine : string = "new one";
 
 let name = "JDJD"
-console.log(name);
+
+let see : string = "sss&";
+// console.log(see);
+// console.log(name);
+
+let num1 = 45;
+let num2 : number = 56;
+
+let bool1 = true;
+let bool2 : boolean = false;
+
+console.log(Boolean(100));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(' '));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean("false"));
+console.log(Boolean('0'));
+console.log(Boolean('null'));
+console.log(Boolean('undefined'));
+
 //}) ()
